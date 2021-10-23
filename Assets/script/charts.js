5f532f2cf6dcaf4f9676b6bed3de3e9520646934
Andrew Ellison <acannonellison@gmail.com>
2021-10-23 13:52:24 -0700

    Added standalone function to format price values. Reorganized utility functions.

--- Assets/script/charts.js
+++ Assets/script/charts.js
@@ -3,27 +3,7 @@ var queryBase = "https://api.coinpaprika.com/v1/tickers/{coinName}/historical?st
 var coinName = "BitCoin";
 var calcsEl = document.querySelector("#section");
 
-// TODO add functionality to calculate the required time step based on the given start date up 
-// Function to determine which day interval to call from coinpaprika so that results are smooth and cover the whole date range.
-function determineInterval(startDate,endDate) {
-  let maxPoints = 900; // Maximum historical points from api call
-  let intervals = [1,7,14,30,90]; // coinpaprika available day intervals
-  let start = moment(startDate,'mm-dd-yyyy');
-  let end = moment(endDate,'mm-dd-yyyy');
-  
-  let diffDays = moment.duration(end.diff(start)).asDays();
-  
-  for (let ii = 0; ii < intervals.length; ii++) {
-    if (diffDays/intervals[ii] < maxPoints) {
-      return intervals[ii]+"d"
-    }
-  }
-
-  // if no matching interval was found default behavior as 14d
-  return "14d"
-
-}
-
+// Dummy historic coinapi call
 function callCoinApi() {
 
   let startDate = "2012-02-15";
@@ -52,28 +32,7 @@ function callCoinApi() {
     
 }
 
-// Pull times and prices from coins or stocks historical data array
-// checks if type is 'coin' or 'stock' to parse the passed in data structure
-function extractData(data,type) {
-  let times = [];
-  let prices = [];
-
-  if (type == 'coin') {
-    data.forEach((entry) => {
-      times.push(entry.timestamp);
-      prices.push(entry.price);
-    })
-  } else if (type == 'stock') {
-    // unsure of stock api data format yet
-    data.forEach((entry) => {
-      times.push(entry.timestamp);
-      prices.push(entry.price);
-    })
-  }
-  return {'times':times,'prices':prices}
-}
-
-// Coin api charting
+// Creating chart of historic stock or coin values
 function displayTicker(values,name,type) {
   console.log('Coin: '+name);
   
@@ -105,6 +64,7 @@ function displayTicker(values,name,type) {
 
 }
 
+// Calculating potential earnings and adding to ui
 function displayCalcs(values,name) {
 
   // pull list of times and prices from extracted data
@@ -112,24 +72,85 @@ function displayCalcs(values,name) {
   let prices = values.prices;
 
   let investAmount = 10000;
-  let result = "$"+((investAmount) * (prices[prices.length-1]/prices[0])).toFixed(2);
+  let result = (investAmount) * (prices[prices.length-1]/prices[0]);
 
   // Add appropriate commas and formatting using regex a la https://www.codegrepper.com/code-examples/javascript/javascript+format+currency+with+commas
-  result = result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  // result = result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  // result = formatPrice(result);
   
   let mainResult = document.createElement("h2");
-  mainResult.innerHTML = "If you had bought $" + investAmount + " of " + name + " on " + moment(times[0]).format('MMMM DD, YYYY') + ", you would have " + result;
+  mainResult.innerHTML = "If you had bought " + formatPrice(investAmount) + " of " + name + " on " + times[0] + ", you would have " + formatPrice(result);
 
   calcsEl.append(mainResult);
 
   calcsEl.append(document.createElement("br"));
 
   let statsMax = document.createElement("p");
-  statsMax.innerHTML = "The best time to sell " + name + " was on "
+  let maxIndex = prices.indexOf(Math.max(...prices));
+
+  statsMax.innerHTML = "The best time to sell " + name + " was on " + times[maxIndex] + " at " + formatPrice(prices[maxIndex]);
+
+  calcsEl.append(statsMax);
 
 }
 
+// Utility Functions
+// Function to determine which day interval to call from coinpaprika so that results are smooth and cover the whole date range.
+function determineInterval(startDate,endDate) {
+  let maxPoints = 900; // Maximum historical points from api call
+  let intervals = [1,7,14,30,90]; // coinpaprika available day intervals
+  let start = moment(startDate,'mm-dd-yyyy');
+  let end = moment(endDate,'mm-dd-yyyy');
+  
+  let diffDays = moment.duration(end.diff(start)).asDays();
+  
+  for (let ii = 0; ii < intervals.length; ii++) {
+    if (diffDays/intervals[ii] < maxPoints) {
+      return intervals[ii]+"d"
+    }
+  }
+
+  // if no matching interval was found default behavior as 14d
+  return "14d"
+
+}
+
+// Function to pull times and prices from coins or stocks historical data array
+// checks if type is 'coin' or 'stock' to parse the passed in data structure
+function extractData(data,type) {
+  let times = [];
+  let prices = [];
+
+  if (type == 'coin') {
+    data.forEach((entry) => {
+      times.push(moment(entry.timestamp,'YYYY-MM-DDThh:mm:ssZ').format('MMMM DD, YYYY'));
+      prices.push(entry.price);
+    })
+  } else if (type == 'stock') {
+    // unsure of stock api data format yet
+    data.forEach((entry) => {
+      times.push(entry.timestamp);
+      prices.push(entry.price);
+    })
+  }
+  return {'times':times,'prices':prices}
+}
+
+// Convert price string or number to formatted string
+function formatPrice(price) {
+  if (typeof price !== "string") {
+    s = String(price.toFixed(2));
+  } else {
+    s = price;
+  }
+
+  console.log(price)
+  console.log(s)
+  return '$' + s.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+// call to dummy api for testing until user button implemented
 callCoinApi();
 
-// Stock api charting
+// Stock api charting: May not need separate section, extract Data should handle discrepancies
 
