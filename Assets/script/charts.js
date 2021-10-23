3acc84582e263e38073d3f7f23c181cd08837cf3
Andrew Ellison <acannonellison@gmail.com>
2021-10-23 13:26:31 -0700

    Changed data extraction to a more extractData function to handle both stocks and coins. Added basic calculations.

--- Assets/script/charts.js
+++ Assets/script/charts.js
@@ -1,7 +1,7 @@
 // Placeholder api call to get workable data
 var queryBase = "https://api.coinpaprika.com/v1/tickers/{coinName}/historical?start=";
 var coinName = "BitCoin";
-var calcsEl = document.querySelector("#section")
+var calcsEl = document.querySelector("#section");
 
 // TODO add functionality to calculate the required time step based on the given start date up 
 // Function to determine which day interval to call from coinpaprika so that results are smooth and cover the whole date range.
@@ -26,23 +26,21 @@ function determineInterval(startDate,endDate) {
 
 function callCoinApi() {
 
-  let startDate = "2018-02-15";
+  let startDate = "2012-02-15";
   let coinSymbol = "btc-bitcoin";
   let interval = determineInterval(moment(startDate,'yyyy-mm-dd').format('mm-dd-yyyy'),moment().startOf('day').format('mm-dd-yyyy'));
 
-  console.log()
-
   let queryUrl = queryBase.replace('{coinName}',coinSymbol) + startDate + "&interval="+interval
-
-  console.log(queryUrl)
-
 
   fetch(queryUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          displayCalcs(data,coinName);
-          displayTicker(data,coinName);
+
+          console.log(data);
+          values = extractData(data,'coin');
+          displayCalcs(values,coinName,'coin');
+          displayTicker(values,coinName,'coin');
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -54,24 +52,40 @@ function callCoinApi() {
     
 }
 
-// Coin api charting
-function displayTicker(data,coinName) {
-  console.log('Coin: '+coinName);
-  console.log(data);
-
-  // pull list of times and prices from data array
+// Pull times and prices from coins or stocks historical data array
+// checks if type is 'coin' or 'stock' to parse the passed in data structure
+function extractData(data,type) {
   let times = [];
   let prices = [];
 
-  data.forEach((entry) => {
-    times.push(entry.timestamp);
-    prices.push(entry.price);
-  })
+  if (type == 'coin') {
+    data.forEach((entry) => {
+      times.push(entry.timestamp);
+      prices.push(entry.price);
+    })
+  } else if (type == 'stock') {
+    // unsure of stock api data format yet
+    data.forEach((entry) => {
+      times.push(entry.timestamp);
+      prices.push(entry.price);
+    })
+  }
+  return {'times':times,'prices':prices}
+}
+
+// Coin api charting
+function displayTicker(values,name,type) {
+  console.log('Coin: '+name);
+  
+  let times = values.times;
+  let prices = values.prices;
+
+  // pull list of times and prices from data array
 
   let chartData = {
     labels: times,
     datasets: [{
-      label: coinName + ' Price History',
+      label: name + ' Price History',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
       data: prices,
@@ -81,11 +95,7 @@ function displayTicker(data,coinName) {
   let config = {
     type: 'line',
     data: chartData,
-    options: {
-      scales: {
-
-      }
-    }
+    options: {}
   };
 
   let myChart = new Chart(
@@ -95,9 +105,27 @@ function displayTicker(data,coinName) {
 
 }
 
-function displayCalcs(data,current) {
+function displayCalcs(values,name) {
 
+  // pull list of times and prices from extracted data
+  let times = values.times;
+  let prices = values.prices;
 
+  let investAmount = 10000;
+  let result = "$"+((investAmount) * (prices[prices.length-1]/prices[0])).toFixed(2);
+
+  // Add appropriate commas and formatting using regex a la https://www.codegrepper.com/code-examples/javascript/javascript+format+currency+with+commas
+  result = result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  
+  let mainResult = document.createElement("h2");
+  mainResult.innerHTML = "If you had bought $" + investAmount + " of " + name + " on " + moment(times[0]).format('MMMM DD, YYYY') + ", you would have " + result;
+
+  calcsEl.append(mainResult);
+
+  calcsEl.append(document.createElement("br"));
+
+  let statsMax = document.createElement("p");
+  statsMax.innerHTML = "The best time to sell " + name + " was on "
 
 }
 
