9d735488b00f39ced361b7d292d1216616b30891
Andrew Ellison <acannonellison@gmail.com>
2021-10-23 12:51:08 -0700

    Added function to check what interval should be called for historic data from coinpaprika.

--- Assets/script/charts.js
+++ Assets/script/charts.js
@@ -1,17 +1,47 @@
 // Placeholder api call to get workable data
-var query = "https://api.coinpaprika.com/v1/tickers/btc-bitcoin/historical?start=2010-02-15&interval=14d";
+var queryBase = "https://api.coinpaprika.com/v1/tickers/{coinName}/historical?start=";
 var coinName = "BitCoin";
+var calcsEl = document.querySelector("#section")
 
-// TODO add functionality to calculate the required time step based on the given start date up until now
-// Possible intervals 5m 10m 15m 30m 45m 1h 2h 3h 6h 12h 24h 1d 7d 14d 30d 90d 365d
+// TODO add functionality to calculate the required time step based on the given start date up 
+// Function to determine which day interval to call from coinpaprika so that results are smooth and cover the whole date range.
+function determineInterval(startDate,endDate) {
+  let maxPoints = 900; // Maximum historical points from api call
+  let intervals = [1,7,14,30,90]; // coinpaprika available day intervals
+  let start = moment(startDate,'mm-dd-yyyy');
+  let end = moment(endDate,'mm-dd-yyyy');
+  
+  let diffDays = moment.duration(end.diff(start)).asDays();
+  
+  for (let ii = 0; ii < intervals.length; ii++) {
+    if (diffDays/intervals[ii] < maxPoints) {
+      return intervals[ii]+"d"
+    }
+  }
 
+  // if no matching interval was found default behavior as 14d
+  return "14d"
+
+}
 
 function callCoinApi() {
 
-  fetch(query)
+  let startDate = "2018-02-15";
+  let coinSymbol = "btc-bitcoin";
+  let interval = determineInterval(moment(startDate,'yyyy-mm-dd').format('mm-dd-yyyy'),moment().startOf('day').format('mm-dd-yyyy'));
+
+  console.log()
+
+  let queryUrl = queryBase.replace('{coinName}',coinSymbol) + startDate + "&interval="+interval
+
+  console.log(queryUrl)
+
+
+  fetch(queryUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
+          displayCalcs(data,coinName);
           displayTicker(data,coinName);
         });
       } else {
@@ -65,6 +95,13 @@ function displayTicker(data,coinName) {
 
 }
 
+function displayCalcs(data,current) {
+
+
+
+}
+
 callCoinApi();
 
 // Stock api charting
+
